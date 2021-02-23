d7c3358fc7263d2ffd428a483dd7e123e11966f7
Samuel Olwe <sam.olwe@gmail.com>
2021-02-23 22:32:24 +0300

    Exported login fields to form

--- src/screens/loginScreen.js
+++ src/screens/loginScreen.js
@@ -1,13 +1,9 @@
 import React, {useEffect, useState} from 'react';
 import {StatusBar} from 'react-native';
+import LoginForm from '../components/forms/loginForm';
 import styled from 'styled-components/native';
-import Input from '../components/inputs/input';
-import CheckBox from '@react-native-community/checkbox';
-import GenericButton from '../components/buttons/genericButton';
 
 export default function LoginScreen({navigation}) {
-  const [remember, setRemember] = useState(false);
-
   const goToScreen = (screen) => {
     navigation.navigate(screen);
   };
@@ -19,26 +15,7 @@ export default function LoginScreen({navigation}) {
         source={require('../assets/bee.png')}
         resizeMode="contain"
       />
-      <Header>Sign In</Header>
-      <Input placeholder="Username" width={300} />
-      <Input placeholder="Password" width={300} secure={true} />
-      <RowContainer width={300}>
-        <RowContainer>
-          <CheckBox
-            disabled={false}
-            value={remember}
-            onValueChange={() =>
-              remember ? setRemember(false) : setRemember(true)
-            }
-            tintColors={{true: '#333333', false: '#333333'}}
-          />
-          <Text>Remember me</Text>
-        </RowContainer>
-        <Button onPress={() => goToScreen('Reset')}>
-          <Text>Forgot Password?</Text>
-        </Button>
-      </RowContainer>
-      <GenericButton title="SIGN IN" onPress={() => goToScreen('Dashboard')} />
+      <LoginForm />
       <Button onPress={() => goToScreen('SignUp')}>
         <Text>Create Account</Text>
       </Button>
@@ -55,24 +32,10 @@ const Container = styled.View`
   width: 100%;
 `;
 
-const RowContainer = styled.View`
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
-  width: ${(props) => props.width || 'auto'};
-`;
-
 const ImageContainer = styled.Image`
   width: 180px;
   height: 180px;
   margin: 5px;
-`;
-
-const Header = styled.Text`
-  font-size: 40px;
-  color: #000000;
-  font-family: 'Roboto-Light';
-  margin: 10px;
 `;
 
 const Button = styled.TouchableOpacity``;
