9abac9df724c6dd5b8551da8543bf53536b428f7
Samuel Olwe <sam.olwe@gmail.com>
2021-02-27 04:00:35 +0300

    Added screens to dashboard

--- src/navigation/dashboard.js
+++ src/navigation/dashboard.js
@@ -1,13 +1,17 @@
 import React, {useState, useEffect} from 'react';
+import {Alert} from 'react-native';
 import {
   DrawerContentScrollView,
   DrawerItemList,
   DrawerItem,
   createDrawerNavigator,
+  DrawerContent,
 } from '@react-navigation/drawer';
 import {createStackNavigator} from '@react-navigation/stack';
 import styled from 'styled-components/native';
 import BodyTab from './bodyTab';
+import ImageContainer from '../components/layouts/imageContainer';
+import IconButton from '../components/buttons/iconButton';
 
 const Drawer = createDrawerNavigator();
 
@@ -23,84 +27,150 @@ const StackNavigator = createStackNavigator();
 
 export default function Dashboard({route, navigation}) {
   return (
-    <Drawer.Navigator initialRouteName="Home">
+    <Drawer.Navigator
+      initialRouteName="Home"
+      drawerContent={(props) => <CustomDrawerContent {...props} />}
+      drawerContentOptions={{
+        activeTintColor: '#000000',
+        activeBackgroundColor: '#e0e0e0',
+        inactiveTintColor: '#000000',
+        inactiveBackgroundColor: '#ffffff',
+        labelStyle: {...label},
+      }}>
       <Drawer.Screen name="Home" component={BodyTab} />
+      <Drawer.Screen name="Account" title="My Account" component={BodyTab} />
+      <Drawer.Screen name="Settings" component={BodyTab} />
     </Drawer.Navigator>
   );
 }
 
 function CustomDrawerContent(props) {
-  //   const confirmLogout = () =>
-  //     Alert.alert(
-  //       "Logout",
-  //       "Are you sure?",
-  //       [
-  //         {
-  //           text: "Cancel",
-  //           onPress: () => console.log("Cancel Pressed"),
-  //           style: "cancel",
-  //         },
-  //         {
-  //           text: "OK",
-  //           onPress: () => {
-  //             console.log("OK Pressed");
-  //             props.navigation.reset({
-  //               index: 0,
-  //               routes: [
-  //                 {
-  //                   name: "Auth",
-  //                   state: {
-  //                     routes: [
-  //                       {
-  //                         name: "Login",
-  //                       },
-  //                     ],
-  //                   },
-  //                 },
-  //               ],
-  //             });
-  //           },
-  //         },
-  //       ],
-  //       { cancelable: false }
-  //     );
+  const customItems = [
+    {
+      label: 'Home',
+      source: require('../assets/home_dashboard.png'),
+    },
+    {
+      label: 'Account',
+      source: require('../assets/account_dashboard.png'),
+    },
+    {
+      label: 'Settings',
+      source: require('../assets/settings.png'),
+    },
+  ];
+  const confirmLogout = () =>
+    Alert.alert(
+      'Logout',
+      'Are you sure?',
+      [
+        {
+          text: 'Cancel',
+          onPress: () => console.log('Cancel Pressed'),
+          style: 'cancel',
+        },
+        {
+          text: 'OK',
+          onPress: () => {
+            console.log('OK Pressed');
+            props.navigation.reset({
+              index: 0,
+              routes: [
+                {
+                  name: 'Auth',
+                  state: {
+                    routes: [
+                      {
+                        name: 'Login',
+                      },
+                    ],
+                  },
+                },
+              ],
+            });
+          },
+        },
+      ],
+      {cancelable: false},
+    );
 
   return (
-    <DrawerContentScrollView
-      {...props}
-      contentContainerStyle={{flex: 1, justifyContent: 'space-between'}}>
-      <DrawerHeaderContainer></DrawerHeaderContainer>
+    <DrawerContentScrollView>
+      <DrawerHeaderContainer>
+        <IconButton
+          onPress={props.navigation.closeDrawer}
+          underlayColor="#eeeeee">
+          <ImageContainer source={require('../assets/close.png')} />
+        </IconButton>
+        <DrawerLabel>Dashboard</DrawerLabel>
+      </DrawerHeaderContainer>
       <DrawerContentContainer>
-        {/* <DrawerItemList {...props} />
+        {customItems.map((_, i) => {
+          return (
+            <DrawerItem
+              label={({focused, color}) => (
+                <DrawerLabel font="Roboto" size={18}>
+                  {_.label}
+                </DrawerLabel>
+              )}
+              icon={({focused, color, size}) => (
+                <ImageContainer
+                  margin={1}
+                  source={_.source}
+                  resizeMode="contain"
+                />
+              )}
+              inactiveBackgroundColor={
+                props.state.routeNames[props.state.index] === _.label
+                  ? '#eeeeee'
+                  : '#ffffff'
+              }
+              onPress={() => props.navigation.navigate(_.label)}
+            />
+          );
+        })}
         <DrawerItem
-          label="Logout"
-          icon={() => (
-            <DrawerIcon source={require("../assets/drawer_logout_icon.png")} />
+          label={({focused, color}) => (
+            <DrawerLabel font="Roboto" size={18}>
+              Logout
+            </DrawerLabel>
+          )}
+          icon={({focused, color, size}) => (
+            <ImageContainer
+              margin={1}
+              source={require('../assets/exit.png')}
+              resizeMode="contain"
+            />
           )}
           onPress={confirmLogout}
-        /> */}
+        />
       </DrawerContentContainer>
     </DrawerContentScrollView>
   );
 }
 
-const DrawerIcon = styled.Image`
-  width: 12.5%;
-  height: 100%;
-`;
+const label = {
+  fontFamily: 'Roboto-Medium',
+  fontSize: 18,
+  textAlign: 'left',
+  color: '#000000',
+  padding: 5,
+};
 
 const DrawerLabel = styled.Text`
-  font-size: 15px;
-  text-align: center;
-  color: #707070;
+  font-family: ${(props) => props.font || 'Roboto-Medium'};
+  font-size: ${(props) => props.size || 20}px;
+  text-align: left;
+  color: #000000;
+  padding: 5px;
 `;
 
 const DrawerHeaderContainer = styled.View`
-  flex: 1;
-  padding: 15px;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
 `;
 
-const DrawerContentContainer = styled.ScrollView`
-  flex-grow: 1;
-  background-color: #ffebee;
+const DrawerContentContainer = styled.View`
+  background-color: #ffffff;
 `;
