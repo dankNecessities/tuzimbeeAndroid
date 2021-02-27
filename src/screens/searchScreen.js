22d1d8b0c38427a8c901b996bd50ed65104c39ce
Samuel Olwe <sam.olwe@gmail.com>
2021-02-27 21:09:58 +0300

    Unify padding

--- src/screens/searchScreen.js
+++ src/screens/searchScreen.js
@@ -132,7 +132,7 @@ const MainContainer = styled.View`
 const Container = styled.ScrollView`
   background-color: #ffffff;
   width: 100%;
-  padding: 10px 10px 0px 10px;
+  padding: 5px;
 `;
 
 const ItemContainer = styled.View`
