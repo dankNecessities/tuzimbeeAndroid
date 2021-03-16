45a031104f51ab34fec2e8d399c1c9174633e209
Samuel Olwe <sam.olwe@gmail.com>
2021-03-16 11:50:52 +0300

    Integrate remember me from storage

--- src/navigation/mainStack.js
+++ src/navigation/mainStack.js
@@ -1,11 +1,30 @@
-import * as React from 'react';
+import React, {useEffect, useState} from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import AuthStack from './authStack';
 import Dashboard from './dashboard';
+import Storage from '../storage/storage';
+import FloatingLoader from '../components/loaders/floatingLoader';
 
 const Stack = createStackNavigator();
 
 export default function MainStack() {
+  const [loggedIn, setLoggedIn] = useState();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    Storage.getRememberMe().then((result) => {
+      if (result) {
+        setLoggedIn(result);
+      } else {
+        setLoggedIn(false);
+      }
+      setLoading(false);
+    });
+  }, []);
+
+  if (loading) {
+    return <FloatingLoader />;
+  }
   return (
     <Stack.Navigator
       initialRouteName="Auth"
@@ -21,16 +40,20 @@ export default function MainStack() {
           fontSize: 40,
         },
       }}>
-      <Stack.Screen
-        name="Auth"
-        component={AuthStack}
-        options={{headerShown: false}}
-      />
-      <Stack.Screen
-        name="Dashboard"
-        component={Dashboard}
-        options={{headerShown: false}}
-      />
+      {loggedIn === false ? (
+        <Stack.Screen
+          name="Auth"
+          component={AuthStack}
+          options={{headerShown: false}}
+        />
+      ) : null}
+      {loggedIn === true ? (
+        <Stack.Screen
+          name="Dashboard"
+          component={Dashboard}
+          options={{headerShown: false}}
+        />
+      ) : null}
       {/* <Stack.Screen
         name="PasswordReset"
         component={PasswordResetStack}
