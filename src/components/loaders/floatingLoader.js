ad41040db1d852403787938e1391bd7713930105
Samuel Olwe <sam.olwe@gmail.com>
2021-03-16 11:10:47 +0300

    Add offset to the loader

---
+++ src/components/loaders/floatingLoader.js
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import {useWindowDimensions} from 'react-native';
+import styled from 'styled-components';
+
+export default function FloatingLoader() {
+  const window = useWindowDimensions();
+  const size = 50;
+  const depth = 50;
+
+  return (
+    <Container left={(window.width - size) / 2 - 10} size={size} depth={depth}>
+      <Loader size="large" color="#4788c7" />
+    </Container>
+  );
+}
+
+const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  border-radius: 25px;
+  background-color: #ffffff;
+  left: ${(props) => props.left}px;
+  bottom: ${(props) => props.depth}px;
+  elevation: 5;
+  position: absolute;
+  width: ${(props) => props.size}px;
+  height: ${(props) => props.size}px;
+`;
+
+const Loader = styled.ActivityIndicator``;
