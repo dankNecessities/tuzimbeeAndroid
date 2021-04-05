7db52ec35286d2a0157a4cb278367be32df09837
Samuel Olwe <sam.olwe@gmail.com>
2021-04-05 22:00:52 +0300

    Added logout util

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,9 +1,31 @@
+import Storage from '../storage/storage';
+
 const Utils = {
   cutArray: (array, chunkSize, chunkArray) => {
     var i, j;
     for (i = 0, j = array.length; i < j; i += chunkSize) {
       chunkArray.push(array.slice(i, i + chunkSize));
     }
+  },
+  redirectToAuth: (navigation) => {
+    Storage.removeAuthToken();
+    Storage.removeOrderData();
+    Storage.removeRememberMe();
+    navigation.reset({
+      index: 0,
+      routes: [
+        {
+          name: 'Auth',
+          state: {
+            routes: [
+              {
+                name: 'Login',
+              },
+            ],
+          },
+        },
+      ],
+    });
   },
 };
 
